57140f92bec6f646d25dedf54997517e550188a0
James Potter <james.potter@gmail.com>
2022-08-04 15:52:30 +0800

    styled

--- hn_leaders/__init__.py
+++ hn_leaders/__init__.py
@@ -2,12 +2,6 @@ from flask import Flask, render_template
 from flask_caching import Cache
 from .feed import get_feed
 
-# config = {
-#     "DEBUG": False,  # True,
-#     "CACHE_TYPE": "SimpleCache",
-#     "CACHE_DEFAULT_TIMEOUT": 300,
-# }
-
 DEBUG = True
 
 config = {
@@ -22,7 +16,7 @@ app.config.from_mapping(config)
 cache = Cache(app)
 
 
-@cache.cached(timeout=3600)
+@cache.cached(timeout=1800)
 def get_feed_cached():
     return get_feed()
 
@@ -30,5 +24,4 @@ def get_feed_cached():
 @app.route("/", methods=["GET"])
 def index():
     comments = get_feed_cached()
-    # return comments
     return render_template("index.html", comments=comments)
--- hn_leaders/feed.py
+++ hn_leaders/feed.py
@@ -7,29 +7,55 @@ MAX_LEADERS = 50
 MAX_FEED_LENGTH = 100
 
 
+def get_karma(username):
+    print("--> getting karma: %s" % username)
+    obj = requests.get(f"http://hn.algolia.com/api/v1/users/{username}").json()
+    return (username, obj["karma"])
+
+
 def get_leaders():
     print("fetching leaders")
     b = requests.get("https://news.ycombinator.com/leaders").content
     soup = BeautifulSoup(b, "html.parser")
-    return [a.text.strip() for a in soup.select("a.hnuser")[:MAX_LEADERS]]
+    leaders = {}
+    for a in soup.select("a.hnuser"):
+        username = a.text.strip()
+        karma_str = a.parent.parent.select("td")[-1].text.strip()
+        karma = int(karma_str) if karma_str else None
+        leaders[username] = karma
+    missing = [u for u, k in leaders.items() if k is None]
+    with ThreadPoolExecutor(max_workers=25) as executor:
+        for username, karma in executor.map(
+            lambda u: list(get_karma(u)), missing
+        ):
+            leaders[username] = karma
+    res = sorted(leaders.items(), key=lambda x: x[1], reverse=True)
+    return res[:MAX_LEADERS]
 
 
-def get_comments(username):
+def get_comments(username, karma):
     print(f"getting comments: {username}")
     ts = int(arrow.get().shift(days=-14).timestamp())
     url = "https://hn.algolia.com/api/v1/search_by_date"
     url += f"?tags=comment,author_{username}"
     url += f"&numericFilters=created_at_i>={ts}"
-    for hit in requests.get(url).json()["hits"]:
+    seen_stories = set()
+    hits = requests.get(url).json()["hits"]  # so first one is kept per story
+    hits.reverse()
+    for hit in hits:
         content = hit["comment_text"]
-        if len(content) < 50:
+        story_id = hit["story_id"]
+        if story_id in seen_stories:
+            continue
+        if len(content) < 30:
             continue
         yield dict(
             created=arrow.get(hit["created_at"]).datetime,
             created_i=hit["created_at_i"],
             author=hit["author"],
+            author_karma=karma,
             content=content,
-            story_id=hit["story_id"],
+            story_id=story_id,
             story_title=hit["story_title"],
             story_url=hit["story_url"],
             comment_url="https://news.ycombinator.com/item?id=%s"
@@ -37,18 +63,16 @@ def get_comments(username):
             parent_url="https://news.ycombinator.com/item?id=%s"
             % hit["parent_id"],
         )
+        seen_stories.add(story_id)
 
 
 def get_feed():
-    usernames = get_leaders()
+    leaders = get_leaders()
     comments = []
-
-    # # TODO
-    # usernames = usernames[:5]
-
     with ThreadPoolExecutor(max_workers=20) as executor:
-        for arr in executor.map(lambda u: list(get_comments(u)), usernames):
+        for arr in executor.map(
+            lambda x: list(get_comments(x[0], x[1])), leaders
+        ):
             comments.extend(arr)
-
     comments.sort(key=lambda c: c["created_i"], reverse=True)
     return comments[:MAX_FEED_LENGTH]
